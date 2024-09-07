2c0e68e320e92835550bed5873fc580975031949
Abdullahi Abdukadir <abdallaabdukadir256@gmail.com>
2024-09-07 16:47:09 +0300

    Add the add method

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("welcome our simple calcualtor " + "");
+
+        System.out.println(add(2,3));
     }
+
+    public static int add(int a, int b) {
+        return  a + b;
+    }
+
 }
